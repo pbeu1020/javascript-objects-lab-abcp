5a2a7204a3b95c8dccdf1af7081c79471e82298a
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 18:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 // deleteFromObjectByKey(object, key) does not modify the original object (it is non-destructive):
 
 function deleteFromObjectByKey(object, key) {
-  
+  var newObj = Object.assign({}, obj)
 }
 
 
