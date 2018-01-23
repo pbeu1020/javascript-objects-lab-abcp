663c83bf598098cbf245f2c0a5f19c99a9630fac
Peter Babeu <mbeenpete@hotmail.com>
2018-01-23 17:43:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+// Objects defines a `recipes` object:
+var recipes = {};
+
+// updateObjectWithKeyAndValue(object, key, value) returns an object with the orignal key value pairs and the new key value pair:
+function updateObjectWithKeyAndValue(object, key, value) {
+  // playlist[artistName]=songTitle;
+  object[key]=value;
+  return object;
+}
+
+// updateObjectWithKeyAndValue(object, key, value) it does not modify the original object, but rather returns a clone with the new data:
+function updateObjectWithKeyAndValue(object, key, value) {
+  var newObject = Object.assign({}, object, key, value);
+  return newObject;
+}
